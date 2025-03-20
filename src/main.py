b3ec731362804ad20990ac968a84ce8523391cfb
jsb24155 <ethan.keyes.2024@uni.strath.uk>
2025-03-20 02:34:12 +0000

    working prototype of spotify playlist track list printing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
 def main():
     print("""Welcome to spotify-ytdlp, a command line script for converting a spotify playlist to individual .mp3 files
 This script is mainly for things like burning your spotify playlists to cds for use in systems without bluetooth or spotify
@@ -6,7 +9,7 @@ a quick note before the script runs:
           1. Please ensure your spotify playlist is public so the script can see it and fetch the tracklist
           2. these playlists are usually very compressed by spotify so may seem smaller when downloaded via them, ensure you have plenty
           of space free on your drive and multiple cds if you plan on burning it
-          3. This script may take a long time to run, please ensure a stable internet connection and and your device is plugged in if its a laptop
+          3. This script requires you to create your own developer app on spotify for developers, can be found at https://developer.spotify.com/dashboard
           """)
     consent = input("Would you like to continue with the script Y/n:")
     while True:
@@ -16,7 +19,35 @@ a quick note before the script runs:
             exit()
         else:
             consent = input("Would you like to continue with the script Y/n:")
-    print("BEGIN!!!")
+        
+    clientId = input("please input your client id found at https://developer.spotify.com/dashboard in the settings page for your specific app: ")
+    clientSecret = input("please input your client secret found at https://developer.spotify.com/dashboard in the settings page for your specific app: ")
+
+    auth_manager = SpotifyClientCredentials(client_id=clientId, client_secret=clientSecret)
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    link = input("input the link to your spotify playlist: ")
+    playlist = sp.playlist_tracks(link)
+    tracks = playlist['items']
+    while playlist['next']:
+        playlist = sp.next(playlist)
+        tracks.extend(playlist['items'])
+    for i in tracks:
+        p = i["track"]
+        print(f"{p["artist"]} - {p["name"]}")
+    
+    consent = input("Does this look correct? Y/n:")
+    while True:
+        if consent == "y" or consent == "Y" or consent == "":
+            break
+        elif consent == "n" or consent == "N":
+            exit()
+        else:
+            consent = input("Does this look correct? Y/n:")
+    
+    
+
+    
 
 
 
